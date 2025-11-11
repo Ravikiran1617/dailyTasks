580d26fd7c50896deec2c9968c3dc04ce14cc7b4
Ravikiran1617 <armoorravikiran1617@gmail.com>
2025-11-11 18:37:50 +0530

    own Package creation task

---
+++ New_package/add_two_numbers.py
@@ -0,0 +1,6 @@
+from Old_package import add_num 
+
+int1 = 20
+int2 = 30
+result =add_num.add_numbers(int1, int2) 
+print(result) 
---
+++ Old_package/__init__.py
@@ -0,0 +1,2 @@
+from .add_num import add_numbers
+from .get_name import get_user_name 
